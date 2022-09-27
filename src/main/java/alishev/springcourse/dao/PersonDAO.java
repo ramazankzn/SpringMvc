c30d32d541fc7f10628f0d0af8eb8b211eb54de1
Ramazan Khakimov <ramazankzn@gmail.com>
2022-09-27 15:11:08 +0300

    Spring 25 lesson DB Connection

--- src/main/java/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/alishev/springcourse/dao/PersonDAO.java
@@ -3,41 +3,86 @@ package alishev.springcourse.dao;
 import alishev.springcourse.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people = new ArrayList<>();;
 
-    {
-        people.add(new Person(++PEOPLE_COUNT, "Rick"));
-        people.add(new Person(++PEOPLE_COUNT, "Ram"));
-        people.add(new Person(++PEOPLE_COUNT, "Serg"));
-        people.add(new Person(++PEOPLE_COUNT, "Rust"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+
+    static  {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person(resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("age"),
+                        resultSet.getString("email"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    //    return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES (" + 1 + ",'" + person.getName()  +
+                    "'," + person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     public void update(int id, Person person) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(person.getName());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(person.getName());
+//        personToBeUpdated.setAge(person.getAge());
+//        personToBeUpdated.setEmail(person.getEmail());
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+       // people.removeIf(p -> p.getId() == id);
     }
 
 }
--- src/main/java/alishev/springcourse/models/Person.java
+++ src/main/java/alishev/springcourse/models/Person.java
@@ -1,15 +1,43 @@
 package alishev.springcourse.models;
 
+import javax.validation.constraints.*;
+
 public class Person {
     public Person() {
     }
 
     private int id;
+    @NotEmpty(message = "Name should nit be empty")
+    @Size(min = 2, max = 30, message = "Name short or long")
     private String name;
+    @Min(value = 0, message = "Can't be negative")
+    @Max(value = 150, message = "Sorry, peoples can'l live so much")
+    private int age;
+    @NotEmpty(message = "E-mail should not be empty")
+    @Email(message = "Should be valid")
+    private String email;
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public int getId() {
