ad707b83d1a0a51de428cca0a0114c660e141421
Ramazan Khakimov <ramazankzn@gmail.com>
2022-09-28 08:56:01 +0300

    Spring 27 lesson Replace properties from config to database.properties.origins

--- src/main/java/alishev/springcourse/config/SpringConfig.java
+++ src/main/java/alishev/springcourse/config/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,17 +17,21 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @ComponentScan("alishev.springcourse")
 @EnableWebMvc
+@PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+    private final Environment environment;
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -56,10 +62,10 @@ public class SpringConfig implements WebMvcConfigurer {
     public DataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 
-        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        driverManagerDataSource.setUsername("postgres");
-        driverManagerDataSource.setPassword("root");
+        driverManagerDataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
+        driverManagerDataSource.setUrl(environment.getProperty("url"));
+        driverManagerDataSource.setUsername(environment.getProperty("username.value"));
+        driverManagerDataSource.setPassword(environment.getProperty("password"));
 
         return driverManagerDataSource;
     }
