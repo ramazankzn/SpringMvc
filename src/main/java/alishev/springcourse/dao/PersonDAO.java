e5a19b976e5170301e36bf58482c004145c64218
Ramazan Khakimov <ramazankzn@gmail.com>
2022-10-03 13:00:44 +0300

    Spring 42-43 lessons Added address pattern validation

--- src/main/java/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/alishev/springcourse/dao/PersonDAO.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 @Component
@@ -30,12 +31,21 @@ public class PersonDAO {
                 .stream().findAny().orElse(null);
     }
 
+    public Optional<Person> show(String email) {
+
+        return jdbcTemplate.query("SELECT * FROM Person WHERE email=?", new Object[]{email},
+                        new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1 , ?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO Person(name, age, email, address) VALUES(?, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail(), person.getAddress());
     }
 
     public void update(int id, Person person) {
-        jdbcTemplate.update("UPDATE Person SET name = ?, age = ?, email = ? WHERE id=?", person.getName(), person.getAge(), person.getEmail(), person.getId());
+        jdbcTemplate.update("UPDATE Person SET name = ?, age = ?, email = ?, address = ? WHERE id=?",
+                person.getName(), person.getAge(), person.getEmail(), person.getAddress(), id);
     }
 
     public void delete(int id) {
@@ -49,7 +59,7 @@ public class PersonDAO {
         long before = System.currentTimeMillis();
 
         for (Person person : people) {
-            jdbcTemplate.update("INSERT INTO Person VALUES(? , ?, ?, ?)", person.getId(), person.getName(), person.getAge(), person.getEmail());
+            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
         }
 
         long after = System.currentTimeMillis();
@@ -63,14 +73,13 @@ public class PersonDAO {
 
         long before = System.currentTimeMillis();
 
-        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?, ?, ?, ?)",
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?, ?, ?)",
                 new BatchPreparedStatementSetter() {
                     @Override
                     public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setInt(1, people.get(i).getId());
-                        ps.setString(2, people.get(i).getName());
-                        ps.setInt(3, people.get(i).getAge());
-                        ps.setString(4, people.get(i).getEmail());
+                        ps.setString(1, people.get(i).getName());
+                        ps.setInt(2, people.get(i).getAge());
+                        ps.setString(3, people.get(i).getEmail());
                     }
 
                     @Override
@@ -89,7 +98,7 @@ public class PersonDAO {
         Random random = new Random();
 
         for (int i = 0; i < 1000; i++) {
-            people.add(new Person(i + 5, "TestName" + i, random.nextInt(60), "test" + i + "@mail.ru"));
+            people.add(new Person(i + 100, "TestName" + i, random.nextInt(80) + 1, "test" + i + "@mail.ru", i + "Str"));
         }
 
         return people;
--- src/main/java/alishev/springcourse/models/Person.java
+++ src/main/java/alishev/springcourse/models/Person.java
@@ -11,17 +11,30 @@ public class Person {
     @Size(min = 2, max = 30, message = "Name short or long")
     private String name;
     @Min(value = 0, message = "Can't be negative")
-    @Max(value = 150, message = "Sorry, peoples can'l live so much")
+    @Max(value = 150, message = "Sorry, peoples can't live so much")
     private int age;
     @NotEmpty(message = "E-mail should not be empty")
     @Email(message = "Should be valid")
     private String email;
+//Страна, Город, Индекс (6 цифр)
+    //Russia, Moscow, 123456
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}", message = "Your address should be in this format: Country, City, 123456 (index)")
+    private String address;
 
-    public Person(int id, String name, int age, String email) {
+    public Person(int id, String name, int age, String email, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public int getAge() {
